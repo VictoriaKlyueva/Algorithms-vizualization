914b4583a028a1812691a01e2b86b239fdef4943
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-04-16 22:35:43 +0700

    Изменена реализация генетического алгоритма

--- genetic_algorithm/genetic_algorithm.js
+++ genetic_algorithm/genetic_algorithm.js
@@ -1,185 +1,222 @@
 const canvas = document.getElementById("canvas_genetic_algorithm");
 const context = canvas.getContext("2d");
-var clicks = 0; //number of clicks
 
-var data = [];
+// Количество кликов по canvas
+var clicks = 0;
 
-function getRandomProbability() {
-  return Math.random() - 0.5;
+let vertecies = [];
+
+const NUM_ITERATIONS = 1000;
+const NUM_POPULATION = 1000;
+const NUM_GENERATIONS = 100000;
+const MUTATION_RATE = 0.3;
+
+let lengthOfChromosome;
+
+function randomNumber(left, right) {
+    return  Math.floor(Math.random() * (right - left) + left);
 }
 
-function geneticAlgorithm(data) {
-  var adjacencyMatrix = getGraph(data);
+function getFirstPopulation(firstGeneration){
+    let res = [];
+    let current = firstGeneration.slice();
+    current.push(distance(current));
+    res.push(current);
 
-  // гиперпараметры
-  const NUM_CITIES = adjacencyMatrix.length;
-  const NUM_POPULATION = 100;
-  const NUM_GENERATIONS = 10000;
-  const MUTATION_RATE = 0.3;
-
-  // Генерация начальной популяции
-  function generateInitialPopulation() {
-    let population = [];
-    for (let i = 0; i < NUM_POPULATION; i++) {
-      population.push([...Array(NUM_CITIES).keys()].sort(() => getRandomProbability()));
+    for (let i = 0; i < Math.pow(vertecies.length, 2); i++) {
+        current = firstGeneration.slice();
+        current = shuffleArray(current)
+        current.push(distance(current));
+        res.push(current)
     }
-    return population;
-  }
 
-  // Оценка приспособленности особей
-  function fitness(individual) {
-    let cost = 0;
-    for (let i = 0; i < NUM_CITIES - 1; i++) {
-      cost += adjacencyMatrix[individual[i]][individual[i + 1]];
+    return res;
+}
+
+function addToPopulation(population, chromosome) {
+    if (!population.length) {
+        population.push(chromosome);
     }
-    cost += adjacencyMatrix[individual[NUM_CITIES - 1]][individual[0]];
-    // Припособленность - обратная велечина стоимости
-    return 1 / cost;
-  }
-
-  // Скрещивание особей
-  function crossing(firstParent, secnodParent) {
-    const start = Math.floor(Math.random() * NUM_CITIES);
-    const end = Math.floor(Math.random() * (NUM_CITIES - start)) + start;
-
-    let child = firstParent.slice(start, end);
-    for (let gene of secnodParent) {
-      if (!child.includes(gene)) {
-        child.push(gene);
-      }
+    else {
+        let flag = false
+        for (let i = 0; i < population.length; i++) {
+            if (chromosome[chromosome.length - 1] < population[i][population[i].length - 1]) {
+                population.splice(i, 0, chromosome);
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            population.push(chromosome);
+        }
     }
+}
+
+function distance(chromosome){
+    let ans = 0;
+    for (let i = 0; i < chromosome.length - 1; i++) {
+        ans += Math.sqrt(Math.pow(chromosome[i][0] - chromosome[i + 1][0], 2) + Math.pow(chromosome[i][1] - chromosome[i + 1][1], 2));
+    }
+    ans += Math.sqrt(Math.pow(chromosome[chromosome.length - 1][0] - chromosome[0][0], 2) + Math.pow(chromosome[chromosome.length - 1][1] - chromosome[0][1], 2));
+    return ans;
+}
+
+function cross(firstParent, secondParent){
+    let child = [];
+
+    let randomSubGen = firstParent.slice(randomNumber(0, firstParent.length), 
+                                     randomNumber(randomNumber(0, firstParent.length) + 1, 
+                                                  firstParent.length) + 1)
+
+    child = randomSubGen;
+
+    for (let num of secondParent) {
+        if (!child.includes(num)) {
+            child.push(num);
+        }
+    }
+
+    if (Math.random() < MUTATION_RATE){
+        let index1 = randomNumber(1, lengthOfChromosome);
+        let index2 = randomNumber(1, lengthOfChromosome)
+
+        if (index1 != index2) {
+            child[index1], child[index2] = child[index2], child[index1];
+        }
+    }
+
     return child;
-  }
-
-  // Мутация
-  function mutation(individual) {
-    if (Math.random() < MUTATION_RATE) {
-      const idx1 = Math.floor(Math.random() * NUM_CITIES);
-      const idx2 = (idx1 + 1) % NUM_CITIES;
-      [individual[idx1], individual[idx2]] = [individual[idx2], individual[idx1]];
-    }
-    return individual;
-  }
-
-  // Основной цикл алгоритма
-  let population = generateInitialPopulation();
-
-  for (let generation = 0; generation < NUM_GENERATIONS; generation++) {
-    let newPopulation = [];
-    for (let i = 0; i < NUM_POPULATION; i++) {
-      // Выбор рандомных родителей
-      let parent1 = population[Math.floor(Math.random() * NUM_POPULATION)];
-      let parent2 = population[Math.floor(Math.random() * NUM_POPULATION)];
-      let child = crossing(parent1, parent2);
-      child = mutation(child);
-      newPopulation.push(child);
-    }
-    population = newPopulation;
-  }
-
-  // Нахождение лучшего решения
-  let bestRoute = population.sort((a, b) => fitness(b) - fitness(a))[0];
-  let bestCost = 0;
-  for (let i = 0; i < NUM_CITIES - 1; i++) {
-    bestCost += adjacencyMatrix[bestRoute[i]][bestRoute[i + 1]];
-  }
-  bestCost += adjacencyMatrix[bestRoute[NUM_CITIES - 1]][bestRoute[0]];
-
-  console.log("Лучший маршрут: ", bestRoute);
-  console.log("Лучшая стоимость: ", bestCost);
-
-  plotLines(data, bestRoute);
 }
 
+function makeChild(firstParent, secondParent) { // 18+
+    let firstChild = cross(firstParent, secondParent);
+    let secondChild = cross(firstParent, secondParent);
 
-function plotLines(data, trail, color='white') {
-  context.strokeStyle = color;
-  context.lineWidth = "7";
-  context.lineCap = "round";
+    firstChild.push(distance(firstChild))
+    secondChild.push(distance(secondChild))
 
-  // Передвигаемся в начальную вершину
-  context.moveTo(data[trail[0]].x, data[trail[0]].y);
+    let child = [firstChild, secondChild];
+    return child;
+}
 
+document.getElementById("genetic_algorithm_button").onclick = geneticAlgorithm;
+async function geneticAlgorithm() {
+    resetProcessing(false);
+    for (let i = 0; i < vertecies.length; i++) {
+        plotDot(vertecies[i][0], vertecies[i][1], 20);
+    }
 
-  // Рисуем рёбра
-  for (let i = 1; i < clicks; i++) {
-    context.lineTo(data[trail[i]].x, data[trail[i]].y);
-    context.stroke();
-  }
+    let firstGeneration = [];
 
-  // Возвращаемся к начальной вершине
-  context.lineTo(data[trail[0]].x, data[trail[0]].y);
-  context.stroke();
+    for (let i = 0; i < vertecies.length; i++) {
+        firstGeneration.push(vertecies[i]);
+    }
 
-  context.closePath();
+    lengthOfChromosome = firstGeneration.length;
+
+    let population = getFirstPopulation(firstGeneration);
+    population.sort((function (a, b) { return a[a.length - 1] - b[b.length - 1]}));
+
+    let bestChromosome = population[0];
+
+    for(let i = 0; i < NUM_GENERATIONS; i++) {
+        if (i === NUM_ITERATIONS) {
+            plotLines(bestChromosome)
+            break;
+        }
+
+        population = population.slice(0, vertecies.length * vertecies.length);
+
+        for (let j = 0; j < vertecies.length * vertecies.length; j++) {
+            let index1 = randomNumber(0, population.length);
+            let index2 = randomNumber(0, population.length);
+
+            let firstParent = population[index1].slice(0, population[index1].length - 1);
+            let secondParent = population[index2].slice(0, population[index2].length - 1);
+
+            let child = makeChild(firstParent, secondParent);
+
+            population.push(child[0])
+            population.push(child[1])
+        }
+
+        population.sort((function (a, b) {
+            return a[a.length - 1] - b[b.length - 1]
+        }));
+
+        if (JSON.stringify(bestChromosome) !== JSON.stringify(population[0])){
+            bestChromosome = population[0].slice();
+        }
+
+        for (let i = 0; i < vertecies.length; i++) {
+            plotDot(vertecies[i][0], vertecies[i][1], 20);
+        }
+    }
 }
 
 function plotDot(x, y, radius, color='white') {
-  context.beginPath();
-
-  context.fillStyle = color; 
-
-  context.arc(x, y, radius, 0, 2 * Math.PI, false);
-
-  context.lineWidth = 5;
-  context.strokeStyle = 'white';
-
-  context.stroke();
-  context.closePath();
-}
-
-// функция для расчета расстояния между двумя точками
-function calculateDistance(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-}
-
-function getGraph(data) {
-  let graph = [];
+    context.beginPath();
   
-  // Инициализируем граф нулями
-  for (let i = 0; i < clicks; i++) {
-    graph[i] = [];
-    for (let j = 0; j < clicks; j++) {
-      graph[i][j] = 0;
-    }
-  }
-
-  // заполняем граф
-  for (let i = 0; i < clicks; i++) {
-    for (let j = i; j < clicks; j++) {
-        graph[i][j] = calculateDistance(data[i].x, data[i].y, data[j].x, data[j].y);
-        graph[j][i] = calculateDistance(data[i].x, data[i].y, data[j].x, data[j].y);
-    }
-  }
-
-  return graph;
+    context.fillStyle = color; 
+  
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  
+    context.lineWidth = 5;
+    context.strokeStyle = 'white';
+  
+    context.stroke();
+    context.closePath();
 }
 
-// расстоновка точек пользователем
+function plotLines(bestPath, color="white") {
+    bestPath.splice(bestPath.length - 1, 0, bestPath[0])
+    console.log(bestPath)
+
+    context.strokeStyle = color;
+    context.lineWidth = "7";
+    context.lineCap = "round";
+
+    context.beginPath();
+    for (let i = 0; i < bestPath.length - 2; i++) {
+        let vector = [bestPath[i + 1][0] - bestPath[i][0] , bestPath[i + 1][1] - bestPath[i][1]];
+        let s = Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+
+        context.moveTo(bestPath[i][0] + vector[0] * 10 / s, bestPath[i][1] + vector[1] * 10 / s);
+        context.lineTo(bestPath[i + 1][0] - vector[0] * 10 / s, bestPath[i + 1][1] - vector[1] * 10 / s);
+
+        context.stroke()
+    }
+}
+
+function shuffleArray(array) {
+    for (let i = 0; i < vertecies.length - 1; i++) {
+        let index1 = randomNumber(1, vertecies.length - 1);
+        let index2 = randomNumber(1, vertecies.length - 1);
+        array[index1], array[index2] = array[index2], array[index1];
+    }
+
+    return array;
+}
+
+// Расстановка точек пользователем
 canvas.addEventListener("click", (e) => {
-  clicks++;
-  const rect = canvas.getBoundingClientRect();
-  // координаты точки
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-  const radius = 20;
+    let x = e.pageX - e.target.offsetLeft;
+    let y = e.pageY - e.target.offsetTop;
 
-  plotDot(x, y, radius);
+    context.beginPath();
 
-  data.push({x: x, y: y})
+    plotDot(x, y, 20);
+
+    vertecies.push([x, y]);
 });
 
-// Запуск адгоритма по нажатию кнопки
-document.getElementById('genetic_algorithm_button').onclick = buttonProcessing;
-function buttonProcessing() {
-  geneticAlgorithm(data);
-}
-
-// Очистка канваса по нажатию кнопки
-document.getElementById('genetic_algorithm_reset').onclick = resetProcessing;
-function resetProcessing() {
-  const context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  data.length = 0
-  clicks = 0;
+// Очистка canvas по нажатию кнопки
+document.getElementById("genetic_algorithm_reset").onclick = resetProcessing;
+function resetProcessing(resetVertecies=true) {
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (resetVertecies) {
+        vertecies = [];
+        clicks = 0;
+    }
 }
