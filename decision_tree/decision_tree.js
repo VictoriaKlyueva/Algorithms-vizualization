4ab45550b666bd30af8cd2dc73215d18c3d90330
vekosek <88042533+VictoriaKlyueva@users.noreply.github.com>
2024-04-16 15:22:59 +0700

    Улучшены верстка и читаемость кода

--- decision_tree/decision_tree.js
+++ decision_tree/decision_tree.js
@@ -2,150 +2,141 @@
 // https://github.com/pbharrin/machinelearninginaction3x
 
 
-function calculateEntropy(dataset) {
-    let counter = {};
-    for (let record of dataset) {
-      let label = record[record.length - 1];
-      if (counter[label]) {
-        counter[label] += 1;
-      } else {
-        counter[label] = 1;
-      }
-    }
+const canvas = document.getElementById("canvas_decision_tree");
+const context = canvas.getContext('2d');
 
-    let entropy = 0.0;
-    for (let key in counter) {
-      let probability = counter[key] / dataset.length;
-      entropy -= probability * Math.log2(probability);
+const width = canvas.width;
+const height = canvas.height;
+
+var tree;
+
+function calculateEntropy(dataset) {
+  let counter = {};
+  for (let record of dataset) {
+    let label = record[record.length - 1];
+    if (counter[label]) {
+      counter[label] += 1;
     }
-    return entropy;
+    else {
+      counter[label] = 1;
+    }
+  }
+
+  let entropy = 0.0;
+  for (let key in counter) {
+    let probability = counter[key] / dataset.length;
+    entropy -= probability * Math.log2(probability);
+  }
+  return entropy;
 }
 
 function splitDataset(dataSet, axis, value) {
-    let retDataSet = [];
-    for (let featVec of dataSet) {
-      if (featVec[axis] == value) {
-        let reducedFeatVec = featVec.slice();
-        reducedFeatVec.splice(axis, 1);
-        retDataSet.push(reducedFeatVec);
-      }
+  let retDataSet = [];
+  for (let featVec of dataSet) {
+    if (featVec[axis] == value) {
+      let reducedFeatVec = featVec.slice();
+      reducedFeatVec.splice(axis, 1);
+      retDataSet.push(reducedFeatVec);
     }
-    return retDataSet;
+  }
+  return retDataSet;
 }
 
 function chooseBestFeatureToSplit(dataset) {
-    let baseEntropy = calculateEntropy(dataset);
-    let bestInfoGain = 0.0;
-    let bestFeature = -1;
+  let baseEntropy = calculateEntropy(dataset);
+  let bestInfoGain = 0.0;
+  let bestFeature = -1;
 
-    let numFeat = dataset[0].length - 1;
-    for (let indx = 0; indx < numFeat; indx++) {
-        let featValues = new Set();
-        for (let record of dataset) {
-            featValues.add(record[indx]);
-        }
-        let featEntropy = 0.0;
-        for (let value of featValues) {
-            let subDataset = splitDataset(dataset, indx, value);
-            let probability = subDataset.length / dataset.length;
-
-            featEntropy += probability * calculateEntropy(subDataset);
-        }
-
-        let infoGain = baseEntropy - featEntropy;
-        if (infoGain > bestInfoGain) {
-            bestInfoGain = infoGain;
-            bestFeature = indx;
-        }
+  let numFeat = dataset[0].length - 1;
+  for (let indx = 0; indx < numFeat; indx++) {
+    let featValues = new Set();
+    for (let record of dataset) {
+      featValues.add(record[indx]);
     }
 
-    return bestFeature;
+    let featEntropy = 0.0;
+    for (let value of featValues) {
+      let subDataset = splitDataset(dataset, indx, value);
+      let probability = subDataset.length / dataset.length;
+
+      featEntropy += probability * calculateEntropy(subDataset);
+    }
+
+    let infoGain = baseEntropy - featEntropy;
+    if (infoGain > bestInfoGain) {
+      bestInfoGain = infoGain;
+      bestFeature = indx;
+    }
+  }
+
+  return bestFeature;
 }
 
 function createTree(dataset, features) {
-    let labels = dataset.map((record) => record[record.length - 1]);
+  let labels = dataset.map((record) => record[record.length - 1]);
 
-    if (labels.filter((label) => label === labels[0]).length === labels.length) {
-        return labels[0];
-    }
+  if (labels.filter((label) => label === labels[0]).length === labels.length) {
+    return labels[0];
+  }
 
-    if (dataset[0].length === 1) {
-        let majorityCount = labels.reduce(function (a, b) {
-            return labels.filter(function (v) {
-                return v === a;
-            }).length >= labels.filter(function (v) {
-                return v === b;
-            }).length ? a : b;
-        });
+  if (dataset[0].length === 1) {
+    let majorityCount = labels.reduce(function (a, b) {
+        return labels.filter(function (v) {
+            return v === a;
+        }).length >= labels.filter(function (v) {
+            return v === b;
+        }).length ? a : b;
+    });
 
-        return majorityCount;
-    }
+    return majorityCount;
+  }
 
-    let bestFeat = chooseBestFeatureToSplit(dataset);
-    let bestFeatLabel = features[bestFeat];
-    let featValues = new Set();
+  let bestFeat = chooseBestFeatureToSplit(dataset);
+  let bestFeatLabel = features[bestFeat];
+  let featValues = new Set();
 
-    for (let record of dataset) {
-        featValues.add(record[bestFeat]);
-    }
+  for (let record of dataset) {
+      featValues.add(record[bestFeat]);
+  }
 
-    let subLabels = features.slice();
-    subLabels.splice(bestFeat, 1);
+  let subLabels = features.slice();
+  subLabels.splice(bestFeat, 1);
 
-    let tree = {};
-    tree[bestFeatLabel] = {};
+  let tree = {};
+  tree[bestFeatLabel] = {};
 
-    for (let value of featValues) {
-        let subDataset = splitDataset(dataset, bestFeat, value);
-        let subTree = createTree(subDataset, subLabels);
-        tree[bestFeatLabel][value] = subTree;
-    }
-    return tree;
+  for (let value of featValues) {
+    let subDataset = splitDataset(dataset, bestFeat, value);
+    let subTree = createTree(subDataset, subLabels);
+    tree[bestFeatLabel][value] = subTree;
+  }
+  return tree;
 }
 
 function predict(inputTree, features, testVec) {
-    function classify(inputTree, testDict) {
-        let key = Object.keys(inputTree)[0];
-        let subtree = inputTree[key];
-        let testValue = testDict[key];
-        delete testDict[key];
-        if (Object.keys(testDict).length === 0) {
-            return subtree[testValue];
-        } else {
-            return classify(subtree[testValue], testDict);
-        }
+  function classify(inputTree, testDict) {
+    let key = Object.keys(inputTree)[0];
+    let subtree = inputTree[key];
+    let testValue = testDict[key];
+    delete testDict[key];
+    if (Object.keys(testDict).length === 0) {
+      return subtree[testValue];
     }
+    else {
+      return classify(subtree[testValue], testDict);
+    }
+  }
 
-    let testDict = {};
-    for (let i = 0; i < features.length; i++) {
-        testDict[features[i]] = testVec[i];
-    }
-    return classify(inputTree, testDict);
+  let testDict = {};
+  for (let i = 0; i < features.length; i++) {
+    testDict[features[i]] = testVec[i];
+  }
+  return classify(inputTree, testDict);
 }
 
 function printTree(tree) {
-    console.log(JSON.stringify(tree, null, 4));
+  console.log(JSON.stringify(tree, null, 4));
 }
-
-/*
-
-let [dataset, features] = createDataset();
-let tree = createTree(dataset, features);
-printTree(tree);
-
-// Пример тестового датасета
-let testVectors = [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1],
-];
-
-for (let vec of testVectors) {
-    let pred = predict(tree, features, vec);
-    console.log(pred);
-}
-*/
 
 function roundedRectPath(x, y, w, h, r) {
   r = (Math.min(w, h) /2  > r) ? r : Math.min(w, h) / 2;
@@ -159,12 +150,6 @@ function roundedRectPath(x, y, w, h, r) {
 // Функция для отображения дерева на странице
 function createCanvasTree(tree) {
   console.log(tree);
-
-  const canvas = document.getElementById("canvas_decision_tree");
-  const context = canvas.getContext('2d');
-
-  const width = canvas.width;
-  const height = canvas.height;
 
   const margin = 50;
   const nodeWidth = 10;
@@ -256,7 +241,7 @@ actualBtn.addEventListener('change', function() {
     console.log("Держимся");
     console.log(dataArray);
 
-    let tree = createTree(dataArray, features);
+    tree = createTree(dataArray, features);
 
     console.log(tree);
     printTree(tree);
@@ -266,3 +251,11 @@ actualBtn.addEventListener('change', function() {
 
   reader.readAsText(this.files[0]);
 });
+
+// Очистка канваса по нажатию кнопки
+document.getElementById('tree_reset').onclick = resetProcessing;
+function resetProcessing() {
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  tree = {};
+}
